63ee05ff6c9b4fcd2345b260b521adb2bc44e3b8
sharmaine.manuel <sharmaine.manuel@intelligentoutsourcing.co.uk>
2018-07-06 23:40:10 +0800

    options model update

--- app/Option.php
+++ app/Option.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Option extends Model
 {
     //
-
+    protected $table = 'options';
+    protected $fillable = ['id', 'question_id', 'text'];
+    
     public function options(){
         return $this->belongsTo(Question::class);
       }
