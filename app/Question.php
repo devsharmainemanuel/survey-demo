3079dc170b21448059f44a13c9ac5d869b72927e
devsharmainemanuel <sharmaine.manuel@intelligentoutsourcing.co.uk>
2018-07-09 16:12:20 +0000

    Apply fixes from StyleCI

--- app/Question.php
+++ app/Question.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     protected $table = 'questions';
-    protected $fillable = [ 'title', 'question_type', 'user_id' ];
+    protected $fillable = ['title', 'question_type', 'user_id'];
 
     protected $casts = [
     'id' => 'array',
