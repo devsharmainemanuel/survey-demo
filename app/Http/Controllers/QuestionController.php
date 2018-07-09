83cf81ffd26f35e44c8485cb76517933f712160d
devsharmainemanuel <sharmaine.manuel@intelligentoutsourcing.co.uk>
2018-07-09 10:32:19 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,10 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class QuestionController extends Controller
 {
     //
-    
 }
--- app/Question.php
+++ app/Question.php
@@ -6,14 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-  protected $table = 'questions';
-  protected $fillable = ['title', 'question_type', 'user_id'];
-  
-  protected $casts = [
+    protected $table = 'questions';
+    protected $fillable = ['title', 'question_type', 'user_id'];
+
+    protected $casts = [
     'id' => 'array',
   ];
-  
-  public function options(){
-    return $this->hasMany(Option::class);
-  }
+
+    public function options()
+    {
+        return $this->hasMany(Option::class);
+    }
 }
