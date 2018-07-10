7b512738ff95e3c599f4fac55c12a92cc22845c9
devsharmainemanuel <sharmaine.manuel@intelligentoutsourcing.co.uk>
2018-07-10 14:59:13 +0000

    Apply fixes from StyleCI

--- app/Option.php
+++ app/Option.php
@@ -10,7 +10,7 @@ class Option extends Model
     protected $table = 'options';
     protected $fillable = ['id', 'question_id', 'text'];
     protected $primaryKey = 'id';
-    
+
     public function options()
     {
         return $this->belongsTo(Question::class);
